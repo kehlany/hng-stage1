8c336b185a2a14723df31b01ed6e876a837f5f5e
kehlany <kelanikhadijat41@gmail.com>
2026-04-19 23:27:45 +0100

    add required system endpoints

--- main.py
+++ main.py
@@ -194,7 +194,7 @@ def health():
 @app.get("/me")
 def me():
     return {
-        "name": "kehlany",
+        "name": "Kelani",
         "email": "kelanikhadijat41@gmail.com",
         "github": "https://github.com/kehlany"
     }
